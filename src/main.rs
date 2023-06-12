cbf733cdfb6116753353120cd790d5a377c1c07a
Kate Goldenring <kate.goldenring@fermyon.com>
2023-06-12 08:48:43 -0700

    Unhide variables commands
    Signed-off-by: Kate Goldenring <kate.goldenring@fermyon.com>

--- src/main.rs
+++ src/main.rs
@@ -17,7 +17,7 @@ enum CloudCli {
     /// Login to Fermyon Cloud
     Login(LoginCommand),
     /// Manage Spin application variables
-    #[clap(subcommand, alias = "vars", hide = true)]
+    #[clap(subcommand, alias = "vars")]
     Variables(VariablesCommand),
 }
 
