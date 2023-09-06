6a22b941c983b81309de9eae9bf03bb2c54cdde6
Vaughn Dice <vaughn.dice@fermyon.com>
2023-09-06 09:44:57 -0600

    fix(build.rs): don't fail on error
    Signed-off-by: Vaughn Dice <vaughn.dice@fermyon.com>

--- build.rs
+++ build.rs
@@ -2,7 +2,6 @@ fn main() {
     vergen::EmitBuilder::builder()
         .git_commit_date()
         .git_sha(true)
-        .fail_on_error()
         .emit()
         .expect("failed to extract build information");
 }
